7d069d64883c90dad38402837149175b1f4bdf09
Tejas Patil <tejaspatil371@gmail.com>
2021-08-17 18:36:49 +0530

    EvenOdd Test class added

---
+++ src/EvenOdd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Number   ");
+		int num=sc.nextInt();
+		if(num%2==0)
+		{
+			System.out.println(num+"  Number is Even");
+		}
+		else
+		{
+			System.out.println(num+"  Number is Odd");
+		}
+
+
+	}
+
+}
